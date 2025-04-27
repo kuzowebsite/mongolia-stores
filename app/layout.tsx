ec241e3b6ff3a84eb28f8490242d59722f43702f
kuzowebsite <129853786+kuzowebsite@users.noreply.github.com>
2025-04-28 02:05:54 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import ClientLayout from "./ClientLayout"
+import "./globals.css"
 
 // Metadata can't be used in client components, so we need to define it separately
 export const metadata: Metadata = {
