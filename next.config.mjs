032026b5bac93cef822003ec60c8adf17023cd66
kuzowebsite <129853786+kuzowebsite@users.noreply.github.com>
2025-04-28 02:11:13 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,15 +7,8 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ['firebasestorage.googleapis.com', 'localhost', 'placeholder.com'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
     unoptimized: true,
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
